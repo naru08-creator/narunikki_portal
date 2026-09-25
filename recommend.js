a4b6dee0e05497d90253f030a95d541e657c42db
naru <ikumi.m.0827@gmail.com>
2026-09-25 15:53:37 +0900

    Add files via upload

--- recommend.js
+++ recommend.js
@@ -22,33 +22,33 @@ const recommendItems = [
 {
   title: "ひも付きロールポリ袋：M（30枚入り）",
   link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272347745&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2611-FKR-07-000%2F%3Fcl%3D01%26b%3D3coins",
-  image: "https://contents.palcloset.jp/static/images/item/693950_2886822_1.jpg",
-  categories: ["おすすめ", "旅行","推し活"],
-  details: "ライブや旅行中、荷物を地面に直接置きたくないときに便利なロールポリ袋。バッグを丸ごと入れたり、旅行中に着用済みの衣類を分けて入れたりと、さまざまな用途に使えます。",
-  shopLabel: "3COINSで見る",
-  isRecommend: true,
-  isAffiliate: true
-},
+    image: "https://contents.palcloset.jp/static/images/item/693950_2886822_1.jpg",
+    categories: ["おすすめ", "旅行","推し活"],
+    details: "ライブや旅行中、荷物を地面に直接置きたくないときに便利なロールポリ袋。バッグを丸ごと入れたり、旅行中に着用済みの衣類を分けて入れたりと、さまざまな用途に使えます。",
+    shopLabel: "3COINSで見る",
+    isRecommend: true,
+    isAffiliate: true
+  },
 {
-  title: "マルチホルダー",
-  link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272432802&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2621-BAND01-000%2F%3Fcl%3D36%26b%3D3coins",
-  image: "https://contents.palcloset.jp/static/images/item/733584_3024624_1.jpg",
-  categories: ["おすすめ","旅行"],
-  details: "旅行中に羽織物を一時的にかけておける便利なアイテム。手に持つ荷物を減らして両手を空けられるので、移動中や荷物が多いときにも活躍します。",
-  shopLabel: "3COINSで見る",
-  isRecommend: true,
-  isAffiliate: true
-},
+    title: "マルチホルダー",
+    link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272432802&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2621-BAND01-000%2F%3Fcl%3D36%26b%3D3coins",
+    image: "https://contents.palcloset.jp/static/images/item/733584_3024624_1.jpg",
+    categories: ["おすすめ","旅行"],
+    details: "旅行中に羽織物を一時的にかけておける便利なアイテム。手に持つ荷物を減らして両手を空けられるので、移動中や荷物が多いときにも活躍します。",
+    shopLabel: "3COINSで見る",
+    isRecommend: true,
+    isAffiliate: true
+  },
 {
-  title: "エレコム モバイルバッテリー コンセント一体型 5000mAh",
-  link: "https://hb.afl.rakuten.co.jp/ichiba/57dda4c2.9fc9a251.57dda4c3.3a68f890/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Felecom%2F4549550420242%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
-  image: "https://hbb.afl.rakuten.co.jp/hgb/57dda4c2.9fc9a251.57dda4c3.3a68f890/?me_id=1193217&item_id=10070830&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Felecom%2Fcabinet%2F2026_01%2Fec-c57lmn_01.jpg%3F_ex%3D240x240&s=240x240&t=picttext",
-  categories: ["おすすめ","旅行"],
-  details: "モバイルバッテリーと充電器が一体になっているので、これ1台でスマホの充電に対応できる便利なアイテム。コンセントからの充電にも使えるので、旅行や遠征時の荷物を減らしたいときにもおすすめです。",
-  shopLabel: "楽天で見る",
-  isRecommend: true,
-  isAffiliate: true
-},
+    title: "エレコム モバイルバッテリー コンセント一体型 5000mAh",
+    link: "https://hb.afl.rakuten.co.jp/ichiba/57dda4c2.9fc9a251.57dda4c3.3a68f890/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Felecom%2F4549550420242%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+    image: "https://hbb.afl.rakuten.co.jp/hgb/57dda4c2.9fc9a251.57dda4c3.3a68f890/?me_id=1193217&item_id=10070830&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Felecom%2Fcabinet%2F2026_01%2Fec-c57lmn_01.jpg%3F_ex%3D240x240&s=240x240&t=picttext",
+    categories: ["おすすめ","旅行"],
+    details: "モバイルバッテリーと充電器が一体になっているので、これ1台でスマホの充電に対応できる便利なアイテム。コンセントからの充電にも使えるので、旅行や遠征時の荷物を減らしたいときにもおすすめです。",
+    shopLabel: "楽天で見る",
+    isRecommend: true,
+    isAffiliate: true
+  },
 ];
 
   /*
