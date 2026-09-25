cf4a3943fe1f83ce0d410ab832fe9f4237835b43
naru <ikumi.m.0827@gmail.com>
2026-09-25 14:19:47 +0900

    Add files via upload

---
+++ recommend.js
@@ -0,0 +1,52 @@
+const recommendItems = [
+  {
+    title: "ドリンクホルダー付きキャリーケース",
+    link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272363100&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2523-HFA205-000%2F%3Fcl%3D19%26b%3D3coins",
+    image: "https://contents.palcloset.jp/static/images/item/676447_2827709_1.jpg",
+    categories: ["おすすめ", "旅行"],
+    details: "旅行や遠征に便利なドリンクホルダー付きキャリーケース。移動中に飲み物を置けるので、荷物が多いときにも使いやすいアイテムです。",
+    shopLabel: "3COINSで見る",
+    isRecommend: true,
+    isAffiliate: true
+  },
+  {
+    title: "衣類圧縮収納ケース：M",
+    link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272425364&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2621-3C7B511-00%2F%3Fcl%3D19%26b%3D3coins",
+    image: "https://contents.palcloset.jp/static/images/item/735863_3031960_1.jpg",
+    categories: ["おすすめ", "旅行"],
+    details: "かさばりやすい衣類をコンパクトにまとめられる圧縮収納ケース。旅行や遠征の荷物整理に便利なアイテムです。",
+    shopLabel: "3COINSで見る",
+    isRecommend: true,
+    isAffiliate: true
+  },
+{
+  title: "ひも付きロールポリ袋：M（30枚入り）",
+  link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272347745&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2611-FKR-07-000%2F%3Fcl%3D01%26b%3D3coins",
+  image: "https://contents.palcloset.jp/static/images/item/693950_2886822_1.jpg",
+  categories: ["おすすめ", "旅行","推し活"],
+  details: "ライブ中に荷物を地べたに直置きしたくない時、バッグを丸ごと入れられて便利。旅行時、一度着て汚れた服を分けて入れておく用にも良さそう。",
+  shopLabel: "3COINSで見る",
+  isRecommend: true,
+  isAffiliate: true
+},
+{
+  title: "マルチホルダー",
+  link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272432802&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2621-BAND01-000%2F%3Fcl%3D36%26b%3D3coins",
+  image: "https://contents.palcloset.jp/static/images/item/733584_3024624_1.jpg",
+  categories: ["おすすめ","旅行"],
+  details: "羽織物を一時的にかけておけるので、かさばる荷物を手で持つ必要がなく、両手が空く優れものです",
+  shopLabel: "3COINSで見る",
+  isRecommend: true,
+  isAffiliate: true
+},
+{
+  title: "エレコム モバイルバッテリー コンセント一体型 5000mAh",
+  link: "https://hb.afl.rakuten.co.jp/ichiba/57dda4c2.9fc9a251.57dda4c3.3a68f890/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Felecom%2F4549550420242%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+  image: "https://hbb.afl.rakuten.co.jp/hgb/57dda4c2.9fc9a251.57dda4c3.3a68f890/?me_id=1193217&item_id=10070830&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Felecom%2Fcabinet%2F2026_01%2Fec-c57lmn_01.jpg%3F_ex%3D240x240&s=240x240&t=picttext",
+  categories: ["おすすめ","旅行"],
+  details: "これ1台でモバイルバッテリーとしても、コンセントからの充電器としても使えるのが本当に便利。Apple WatchとiPhoneなど複数台を同時に充電できるのも大きな魅力です。",
+  shopLabel: "楽天で見る",
+  isRecommend: true,
+  isAffiliate: true
+},
+];
