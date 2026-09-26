19095742a6b760c59f18ea6aa88f830cd1b28430
naru <ikumi.m.0827@gmail.com>
2026-09-26 15:13:08 +0900

    Add files via upload

--- recommend.js
+++ recommend.js
@@ -1,53 +1,127 @@
 const recommendItems = [
-/*  
+ 
 {
     title: "ドリンクホルダー付きキャリーケース",
-    link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272363100&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2523-HFA205-000%2F%3Fcl%3D19%26b%3D3coins",
-    image: "https://contents.palcloset.jp/static/images/item/676447_2827709_1.jpg",
+    image: "https://hbb.afl.rakuten.co.jp/hgb/57e42ea2.a2c44a12.57e42ea4.64694a56/?me_id=1306996&item_id=10004491&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fpremium-interior%2Fcabinet%2Ftravel%2Flff-vialink008%2Flff-vialink008_tum1.jpg%3F_ex%3D240x240&s=240x240&t=pict",
     categories: ["おすすめ", "旅行"],
     details: "旅行や遠征に便利なドリンクホルダー付きキャリーケース。移動中に飲み物を置けるので、荷物が多いときにも使いやすいアイテムです。",
-    shopLabel: "3COINSで見る",
+    shopLinks: [
+    {
+     label: "楽天で見る",
+     url: "https://hb.afl.rakuten.co.jp/ichiba/57e42ea2.a2c44a12.57e42ea4.64694a56/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fpremium-interior%2Flff-vialink008%2F&link_type=pict&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0Iiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MSwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9",
+    },
+    {
+     label: "Amazonで見る",
+     url: "https://www.amazon.co.jp/s?k=%E3%83%89%E3%83%AA%E3%83%B3%E3%82%AF%E3%83%9B%E3%83%AB%E3%83%80%E3%83%BC%E4%BB%98%E3%81%8D%E3%82%AD%E3%83%A3%E3%83%AA%E3%83%BC%E3%82%B1%E3%83%BC%E3%82%B9&crid=2BOO003A0OG69&sprefix=%2Caps%2C1654&linkCode=ll2&tag=erupyon-22&linkId=fe2808808e5d0ab1434a6944dfd22387&ref_=as_li_ss_tl",
+    },
+    {
+    label: "Yahoo!で見る",
+    url: "https://yahoo.jp/YXM4Sw",
+    },
+],
     isRecommend: true,
     isAffiliate: true
   },
+{
+    title: "キャリーケースに後付けできるドリンクホルダー",
+    image: "https://hbb.afl.rakuten.co.jp/hgb/57e443df.507977d3.57e443e0.32e904fe/?me_id=1246341&item_id=10002081&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Forange58%2Fcabinet%2F10040990%2Fimgrc0098182462.jpg%3F_ex%3D240x240&s=240x240&t=pict",
+    categories: ["おすすめ","旅行"],
+    details: "移動中に飲み物を置けるので、荷物が多いときにも使いやすいアイテムです。",
+    shopLinks: [
+    {
+     label: "楽天で見る",
+     url: "https://hb.afl.rakuten.co.jp/ichiba/57e443df.507977d3.57e443e0.32e904fe/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Forange58%2Fdorh%2F&link_type=pict&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0Iiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9",
+    },
+    {
+     label: "Amazonで見る",
+     url: "https://www.amazon.co.jp/s?k=%E3%83%89%E3%83%AA%E3%83%B3%E3%82%AF%E3%83%9B%E3%83%AB%E3%83%80%E3%83%BC+%E3%82%AD%E3%83%A3%E3%83%AA%E3%83%BC%E3%82%B1%E3%83%BC%E3%82%B9+%E5%BE%8C%E4%BB%98%E3%81%91&crid=1AS70HWPL1UET&sprefix=%2Caps%2C2193&linkCode=ll2&tag=erupyon-22&linkId=bfa7b0165076d03fd2bba2e7a89f49f2&ref_=as_li_ss_tl",
+    },
+    {
+     label: "Yahoo!で見る",
+     url: "https://yahoo.jp/SBDSU9",
+    },
+    {
+     label: "ダイソーで見る",
+     url: "https://jp.daisonet.com/products/4550480838038?_pos=19&_sid=7576890db&_ss=r",
+    },
+],   
+    isRecommend: true,
+    isAffiliate: true
+},
   {
-    title: "衣類圧縮収納ケース：M",
-    link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272425364&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2621-3C7B511-00%2F%3Fcl%3D19%26b%3D3coins",
-    image: "https://contents.palcloset.jp/static/images/item/735863_3031960_1.jpg",
+    title: "衣類圧縮収納ケース",
+    image: "https://hbb.afl.rakuten.co.jp/hgb/57e521b2.9bd4ef98.57e521b3.a7907bdf/?me_id=1382613&item_id=10000010&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fvalue-create%2Fcabinet%2Ftravelpouch%2F0328_2.jpg%3F_ex%3D240x240&s=240x240&t=pict",
     categories: ["おすすめ", "旅行"],
     details: "かさばりやすい衣類をコンパクトにまとめられる圧縮収納ケース。旅行や遠征の荷物整理に便利なアイテムです。",
-    shopLabel: "3COINSで見る",
+    shopLinks: [
+    {
+     label: "楽天で見る",
+     url: "https://hb.afl.rakuten.co.jp/ichiba/57e521b2.9bd4ef98.57e521b3.a7907bdf/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fvalue-create%2Ftravel-pouch%2F&link_type=pict&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0Iiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9",
+    },
+    {
+    label: "Amazonで見る",
+    url: "https://www.amazon.co.jp/s?k=%E3%83%88%E3%83%A9%E3%83%99%E3%83%AB+%E5%9C%A7%E7%B8%AE%E3%82%B1%E3%83%BC%E3%82%B9&crid=29UJFZFHO6FV7&sprefix=%2Caps%2C307&linkCode=ll2&tag=erupyon-22&linkId=51ee233062888c257c060800ab89234b&ref_=as_li_ss_tl",
+    },
+    {
+    label: "Yahoo!で見る",
+    url: "https://yahoo.jp/6W28Tw",
+    },
+    {
+     label: "ダイソーで見る",
+     url: "https://jp.daisonet.com/products/4549131909074?srsltid=AU7gw4UD6QMShcvit-rjcuZf_Q6in8iOXT4DfKbStI5Wn3-bSVDtd541",
+    },
+],
     isRecommend: true,
     isAffiliate: true
   },
   {
     title: "ひも付きロールポリ袋：M（30枚入り）",
-    link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272347745&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2611-FKR-07-000%2F%3Fcl%3D01%26b%3D3coins",
-    image: "https://contents.palcloset.jp/static/images/item/693950_2886822_1.jpg",
+    image: null,
     categories: ["おすすめ", "旅行", "推し活"],
     details: "ライブや旅行中、荷物を地面に直接置きたくないときに便利なロールポリ袋。バッグを丸ごと入れたり、旅行中に着用済みの衣類を分けて入れたりと、さまざまな用途に使えます。",
-    shopLabel: "3COINSで見る",
+    shopLinks: [
+        {    
+          label: "3COINSで見る",
+          url: "https://www.palcloset.jp/display/item/2611-FKR-07-000/?cl=01&amp;b=3coins&amp;ss=",
+        },
+ ],
     isRecommend: true,
-    isAffiliate: true
+    isAffiliate: false
   },
   {
     title: "マルチホルダー",
-    link: "https://click.linksynergy.com/link?id=J7bSfXoBpdM&offerid=1293176.5034272432802&type=2&murl=https%3A%2F%2Fwww.palcloset.jp%2Fdisplay%2Fitem%2F2621-BAND01-000%2F%3Fcl%3D36%26b%3D3coins",
-    image: "https://contents.palcloset.jp/static/images/item/733584_3024624_1.jpg",
+    image: null,
     categories: ["おすすめ", "旅行"],
     details: "旅行中に羽織物を一時的にかけておける便利なアイテム。手に持つ荷物を減らして両手を空けられるので、移動中や荷物が多いときにも活躍します。",
-    shopLabel: "3COINSで見る",
+    shopLinks: [
+        {    
+          label: "3COINSで見る",
+          url: "https://www.palcloset.jp/display/item/2621-BAND01-000/?srsltid=AU7gw4U693-xLCaUBBpo1f6wlm-ayMvqmuUOvV7FnRVF9DnblbZ9pGeR",
+        },
+ ],
     isRecommend: true,
-    isAffiliate: true
+    isAffiliate: false
   },
-*/
+
   {
     title: "エレコム モバイルバッテリー コンセント一体型 5000mAh",
-    link: "https://hb.afl.rakuten.co.jp/ichiba/57dda4c2.9fc9a251.57dda4c3.3a68f890/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Felecom%2F4549550420242%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
     image: "https://hbb.afl.rakuten.co.jp/hgb/57dda4c2.9fc9a251.57dda4c3.3a68f890/?me_id=1193217&item_id=10070830&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Felecom%2Fcabinet%2F2026_01%2Fec-c57lmn_01.jpg%3F_ex%3D240x240&s=240x240&t=picttext",
     categories: ["おすすめ", "旅行"],
     details: "モバイルバッテリーと充電器が一体になっているので、これ1台でスマホの充電に対応できる便利なアイテム。コンセントからの充電にも使えるので、旅行や遠征時の荷物を減らしたいときにもおすすめです。",
-    shopLabel: "楽天で見る",
+    shopLinks: [
+        {
+          label: "楽天で見る",
+          url: "https://hb.afl.rakuten.co.jp/ichiba/57dda4c2.9fc9a251.57dda4c3.3a68f890/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Felecom%2F4549550420242%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+         },
+        {
+          label: "Amazonで見る",
+          url: "https://www.amazon.co.jp/%E3%82%A8%E3%83%AC%E3%82%B3%E3%83%A0-%E3%83%A2%E3%83%90%E3%82%A4%E3%83%AB%E3%83%90%E3%83%83%E3%83%86%E3%83%AA%E3%83%BC-AC%E5%85%85%E9%9B%BB%E5%99%A8%E4%B8%80%E4%BD%93%E5%9E%8B-Switch%E5%AF%BE%E5%BF%9C-EC-C57LMN/dp/B0H3TM9RH6?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=3TB9TVTYWG99U&dib=eyJ2IjoiMSJ9.iqvjQzYhvHervlfX71MIJsMx7SA2pBx0QSmoszUseEx3b7WXJ-2zn7FbSJjdkaE7IDiXRK4iwMwG4H-qUsyPQeGHLq4iDO3KaYUMShSVcWmhWbkQNmiiah9ukS0_yuYc8J5QwhaOrkLrpw6Hjc3vJcXPm2mIH0sahHTtad9aQ0TOSH8F0lTfTeDYIQkjNE0T428O3lPIcH1O9IDDgTHATDytchC-flKKqHWnYHd5y9JGbDsTLK9SQzYW0g0uRBxgg7HwKbTIJWcr9OD-Vu_e7fOt_YPWL17yGIqgPcuG6t0.13Hb1O-MFTKLGPGQP-8uSG8pEKYzvM2VEyTpaoGOvlM&dib_tag=se&keywords=%E3%82%B3%E3%83%B3%E3%82%BB%E3%83%B3%E3%83%88%E4%BB%98%E3%81%8D%E3%83%A2%E3%83%90%E3%82%A4%E3%83%AB%E3%83%90%E3%83%83%E3%83%86%E3%83%AA%E3%83%BC&qid=1790139035&sprefix=%E3%82%B3%E3%83%B3%E3%82%BB%E3%83%B3%E3%83%88%E4%BB%98%E3%81%8D%E3%83%A2%E3%83%90%E3%82%A4%E3%83%AB%E3%83%90%E3%83%83%E3%83%86%E3%83%AA%E3%83%BC%2Caps%2C249&sr=8-12-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9tdGY&psc=1&linkCode=ll2&tag=erupyon-22&linkId=49f7104ab660aae8eb666ca69cee26a5&ref_=as_li_ss_tl",
+         },
+        {
+          label: "Yahoo!で見る",
+          url: "https://yahoo.jp/PA2_CJ",
+         },
+],
     isRecommend: true,
     isAffiliate: true
   },
